1bb3d2fabb319d5dbb8c01a825df5868ad442e4d
sugite <1140625494@qq.com>
2014-10-15 17:00:16 +0800

    杨辉三角

---
+++ Pascal's Triangle/main.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int> > generate(int numRows) {
+        vector<vector<int> > res ;
+        for(int i = 0 ; i < numRows ; ++i){
+            vector<int> t = getRow(i) ; 
+            res.push_back(t);
+        }
+        return res ;
+    }
+    
+    vector<int> getRow(int rowIndex) {
+        vector<int> res(rowIndex+1) ;
+        for(int i = 0 ; i <= rowIndex/2 ; ++i ){
+            res[i] = c(rowIndex,i);
+        }
+        for(int i = rowIndex ; i >rowIndex/2 ; i--){
+            res[i] = res[rowIndex - i];
+        }
+        return res;
+    }
+    
+    int c(int n,int k){
+        if(k==0) return 1 ;
+        int up = 1 ;
+        int down = 1 ;
+        int fator = 1;
+        for(int i = 0 ; i < k ; i++){
+            up*= (n-i);
+            down*= (k-i);
+            fator = gcd(up,down);
+            up/=fator;
+            down/=fator;
+        }
+        return up/down ;
+    }
+    
+    int gcd(int a , int b ){
+        if(b==0) return a ;
+        return gcd(b,a%b);
+    }
+
+};
