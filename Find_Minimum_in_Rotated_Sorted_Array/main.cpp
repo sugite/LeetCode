a9c54a9a9b739583058308faf9bf5d28a942d077
sugite <1140625494@qq.com>
2014-10-16 12:58:30 +0800

    右旋有序数组的二分查找

---
+++ Find_Minimum_in_Rotated_Sorted_Array/main.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        if(num.size()<2 || num[0]<num[num.size()-1]) return num[0];
+        int low = 0 ; 
+        int high = num.size()-1 ;
+        while(low<high-1){
+            int mid = (low+high)/2;
+            if(num[low]<=num[mid] && num[mid]<=num[high]){
+                return num[low];
+            }else{
+                if(num[low]<num[mid])
+                    low = mid ;
+                else
+                    high = mid ;    
+            }
+        }
+        return min(num[low],num[high]);
+    }
+};
