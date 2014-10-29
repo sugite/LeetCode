9b5e09fd28ee365fec7c33824d8ee70f2d9932a9
sugite <1140625494@qq.com>
2014-10-29 09:46:46 +0800

    根到叶子路径

--- Path_Sum/main.cpp
+++ Path_Sum/main.cpp
@@ -10,14 +10,9 @@
 class Solution {
 public:
     bool hasPathSum(TreeNode *root, int sum) {
-        if(root==NULL) return false;
-        if(root&&root->left==NULL&&root->right==NULL)
-            return sum==root->val;
-        if(root->left&&root->right)
-            return hasPathSum(root->left,sum-root->val)|| \
-                    hasPathSum(root->right,sum-root->val);
-        if(root->left)
-            return hasPathSum(root->left,sum-root->val);
-        return hasPathSum(root->right,sum-root->val);
+        if(root==NULL) return false ;
+        if(root->val == sum && root->left==NULL && root->right==NULL)
+            return true;
+        return hasPathSum(root->left,sum-root->val) | hasPathSum(root->right,sum-root->val);
     }
 };
