d95e1f7d9d3d5f28888b3bc708626fc028ded7b7
sugite <1140625494@qq.com>
2014-11-02 11:43:32 +0800

    根到叶子最大距离

---
+++ Maximum_Depth_of_Binary_Tree/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if(root==NULL) return 0;
+        if(root->left==NULL && root->right==NULL) return 1;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));
+    }
+};
