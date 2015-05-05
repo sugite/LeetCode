fae26a59eea70807a2564d8db9716ee52f7042be
sugite <1140625494@qq.com>
2015-05-05 18:48:13 +0800

    反转单链表

---
+++ Reverse_Linked_List/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* result = NULL;
+        while(head){
+            ListNode *tmp = head->next;
+            head->next = result;
+            result = head;
+            head = tmp;
+        }
+        return result;
+    }
+};
